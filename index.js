04759b80c3be52f4a6b6749fce51d707f1e50cdc
lavish gehlod <lavishgehlod210204@acropolis.in>
2024-01-27 15:38:24 +0530

    Weather app host

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express();
+const hbs = require('hbs')
+const path = require('path')
+const requests = require('requests');
+
+app.set('view engine','hbs')
+    // console.log()
+hbs.registerPartials(path.join(__dirname,'./views/partials'))
+const imgpaths = path.join(__dirname,'/viwes/partials/img/sun.gif');
+app.get('/',(req,res)=>{
+    res.render('index',{})
+})
+app.get('/home',(req,res)=>{
+    res.render('index',{})
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{})
+})
+app.get('/weather',(req,res)=>{
+    res.render('weather',{})
+})
+console.log(path.join(__dirname,'/viwes/partials/img'))
+app.listen(3000,'127.0.0.1',()=>{
+    console.log('start')
+})
